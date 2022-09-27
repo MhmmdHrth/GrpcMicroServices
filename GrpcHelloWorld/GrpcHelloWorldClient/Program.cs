7116e205684218d1b6c5be1760abe5cf5f19d797
harith781 <harith781@gmail.com>
2022-09-28 01:27:40 +0800

    add grpc client to consume from grpc server

---
+++ GrpcHelloWorld/GrpcHelloWorldClient/Program.cs
@@ -0,0 +1,23 @@
+﻿using Grpc.Net.Client;
+using System;
+using System.Threading.Tasks;
+
+namespace GrpcHelloWorldClient
+{
+    internal class Program
+    {
+        static async Task Main(string[] args)
+        {
+            string serverAddress = "https://localhost:5001";
+
+            using (var channel = GrpcChannel.ForAddress(serverAddress))
+            {
+                var client = new HelloService.HelloServiceClient(channel);
+                var reply = await client.SayHelloAsync(new HelloRequest { Name = "Muhammad Harith" });
+
+                Console.WriteLine($"Greetings: {reply.Message}");
+                Console.ReadLine();
+            }
+        }
+    }
+}
