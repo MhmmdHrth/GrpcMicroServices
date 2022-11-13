3cdc634840d1cf717a048552000f5afdd535b99e
harith781 <harith781@gmail.com>
2022-11-14 00:21:27 +0800

    add discount grpc

---
+++ GrpcHelloWorld/DiscountGrpc/Data/DiscountContext.cs
@@ -0,0 +1,15 @@
+﻿using DiscountGrpc.Models;
+using System.Collections.Generic;
+
+namespace DiscountGrpc.Data
+{
+    public class DiscountContext
+    {
+        public static readonly IEnumerable<Discount> Discounts = new List<Discount>
+        {
+                new Discount { DiscountId = 1, Amount = 100, Code = "CODE_100"},
+                new Discount { DiscountId = 2, Amount = 200, Code = "CODE_200"},
+                new Discount { DiscountId = 3, Amount = 300, Code = "CODE_300"}
+        };
+    }
+}
---
+++ GrpcHelloWorld/DiscountGrpc/Services/DiscountService.cs
@@ -0,0 +1,33 @@
+﻿using DiscountGrpc.Data;
+using DiscountGrpc.Protos;
+using Grpc.Core;
+using Microsoft.Extensions.Logging;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DiscountGrpc.Services
+{
+    public class DiscountService : DiscountProtoService.DiscountProtoServiceBase
+    {
+        private readonly ILogger<DiscountService> _logger;
+
+        public DiscountService(ILogger<DiscountService> logger)
+        {
+            _logger = logger;
+        }
+
+        public override Task<DiscountModel> GetDiscount(GetDiscountRequest request, ServerCallContext context)
+        {
+            var discount = DiscountContext.Discounts.FirstOrDefault(x => x.Code.Equals(request.DiscountCode));
+
+            _logger.LogInformation($"Discount is operated with the {discount.Code} code adn the amount is: {discount.Amount}");
+
+            return Task.FromResult(new DiscountModel
+            {
+                DiscountId = discount.DiscountId,
+                Amount = discount.Amount,
+                Code = discount.Code
+            });
+        }
+    }
+}
